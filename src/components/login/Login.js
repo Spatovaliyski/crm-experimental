d89a6564cd3558337a98620f663cfd97b747040e
Martin Spatovaliyski <martinspatovaliyski@gmail.com>
2021-06-11 12:36:18 +0300

    Login: Auth adjustments

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -18,42 +18,57 @@ async function loginUser(credentials) {
 }
 
 const authList = require('../../auth.json')
-const myObject = JSON.parse(JSON.stringify(authList));
+const loginList = JSON.parse(JSON.stringify(authList));
 
-console.log(myObject['token'][0]['user']);
+{Object.keys(loginList['token'][0]['user']['password']).map((key, i) => (
+  console.log(loginList['token'][0]['user']['password'][key])
+))}
 
 export default function Login({ setToken }) {
 const [username, setUserName] = useState();
 const [password, setPassword] = useState();
 
-console.log(username)
-
 const handleSubmit = async e => {
-  if (username == myObject['token'][0]['user']['email'] && password == myObject['token'][0]['user']['password']) {
-    e.preventDefault();
-    const token = await loginUser({
-      username,
-      password
-    });
-    
-    setToken(token);
+  for(let i = 0; i < loginList['token'][0]['user'].length; i++){
+    if (username == loginList['token'][i]['user']['email'] && password == loginList['token'][i]['user']['password']) {
+      e.preventDefault();
+      const token = await loginUser({
+        username,
+        password
+      });
+      
+      setToken(token);
+    } else {
+      const element = document.getElementByClassName("wrong-credentials");
+      element.classList.add("is-visible");
+      alert("TEST");
+    }
   }
 }
 
   return(
-    <form className="auth-form" onSubmit={handleSubmit}>
-    <div className="auth-field">
-        <label for="username">Email</label>
-        <input type="text" name="username" onChange={e => setUserName(e.target.value)} />
+    <div className="login-box">
+      <div className="login-half">
+        <div className="login-container">
+          <h1 className="login-title">Martin's CRM</h1>
+          <form className="auth-form" onSubmit={handleSubmit}>
+          <div className="auth-field">
+              <label for="username">Email</label>
+              <input type="text" name="username" value="admin2@test.com" onChange={e => setUserName(e.target.value)} />
+          </div>
+          <div className="auth-field">
+              <label for="password">Password</label>
+              <input type="password" name="password" value="admin" onChange={e => setPassword(e.target.value)} />
+              <div className="wrong-credentials">Wrong credentials! Please try again.</div>
+          </div>
+          <div className="auth-field">
+              <button className="button button-primary button-login" type="submit">Log in</button>
+          </div>
+          </form>
+        </div>
+      </div>
+      <div className="login-half"></div>
     </div>
-    <div className="auth-field">
-        <label for="password">Password</label>
-        <input type="password" name="password" onChange={e => setPassword(e.target.value)} />
-    </div>
-    <div className="auth-field">
-        <button className="button button-primary button-login" type="submit">Log in</button>
-    </div>
-    </form>
   )
 }
 
